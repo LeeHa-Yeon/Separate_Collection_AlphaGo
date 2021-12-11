a64927f296ddf3ce67b7e4ad5b8899b9648a4ec1
mumu <tnddls2ek@naver.com>
2021-12-11 17:09:40 +0900

    Initial Commit

---
+++ SeparateCollectionAlphaGo/SeparateCollectionAlphaGo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeparateCollectionAlphaGo
+//
+//  Created by 이숭인 on 2021/12/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
