19c3cbdd44d6005e749b7cb22cff9e5b6a78b1a5
mumu <tnddls2ek@naver.com>
2021-12-15 17:50:57 +0900

    초기 환경 세팅

---
+++ SeparateCollectionAlphaGo/SeparateCollectionAlphaGo/Source/View/SeparateCollectionGuide/Controller/SeparateCollectionGuideViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SeparateCollectionGuideViewController.swift
+//  SeparateCollectionAlphaGo
+//
+//  Created by 이숭인 on 2021/12/15.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class SeparateCollectionGuideViewController: UIViewController {
+    //MARK: - Properties
+    
+    //MARK: - UI Components
+    
+    //MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .darkGray
+        
+        setUI()
+    }
+    
+    //MARK: - Functions
+    func setUI(){
+        
+    }
+}
