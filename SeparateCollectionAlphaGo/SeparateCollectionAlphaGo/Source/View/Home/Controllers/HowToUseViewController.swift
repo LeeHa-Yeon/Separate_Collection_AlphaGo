9555a1c27edb48fb6c36d83092b382c7a21a6d08
LeeHa-Yeon <gkdus1613@naver.com>
2021-12-16 08:36:09 +0900

    [FEAT] 홈화면 구현, 다른 화면과 연결

---
+++ SeparateCollectionAlphaGo/SeparateCollectionAlphaGo/Source/View/Home/Controllers/HowToUseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HowToUseViewController.swift
+//  SeparateCollectionAlphaGo
+//
+//  Created by 이하연 on 2021/12/16.
+//
+
+import UIKit
+
+class HowToUseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
