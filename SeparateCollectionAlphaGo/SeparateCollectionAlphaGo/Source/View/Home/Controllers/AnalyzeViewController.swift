e0160da1bfc69ec574e4d8893fa052ff4b7d4746
LeeHa-Yeon <gkdus1613@naver.com>
2021-12-16 10:04:55 +0900

    [FEAT] 분석화면 초기 UI 설정 완료

--- SeparateCollectionAlphaGo/SeparateCollectionAlphaGo/Source/View/Home/Controllers/AnalyzeViewController.swift
+++ SeparateCollectionAlphaGo/SeparateCollectionAlphaGo/Source/View/Home/Controllers/AnalyzeViewController.swift
@@ -6,25 +6,97 @@
 //
 
 import UIKit
+import Then
+import SnapKit
+import Charts
 
 class AnalyzeViewController: UIViewController {
     
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    // MARK: - Declaration & Definitions
+    let imagePredictor = ImagePredictor()
+    let predictionsToShow = 6
+    
+    // MARK: - Components
+    lazy var imgViewFrame = UIView().then {
+        $0.backgroundColor = .clear
+        $0.layer.borderWidth = 2.0
+    }
+    
+    lazy var photoImg = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+        $0.clipsToBounds = true
+    }
+    
+    lazy var explanationLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 18, weight: .semibold)
+        $0.numberOfLines = 0
+    }
+    
+    lazy var resultView = UIView().then {
+        $0.backgroundColor = .gray
+        $0.alpha = 0.5
+    }
+    
+    lazy var predictionLabel = UILabel().then {
+        $0.text = "Predictions go here"
+        $0.font = .systemFont(ofSize: 12, weight: .semibold)
+        $0.numberOfLines = 0
+    }
+    
+    lazy var barChartView = BarChartView().then {
+        $0.noDataText = "데이터가 없습니다."
+        $0.noDataFont = .systemFont(ofSize: 20)
+        $0.noDataTextColor = .lightGray
+        $0.backgroundColor = .gray
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
     }
-    */
-
+    
+    // MARK: - Functions
+    func setUI(){
+        view.backgroundColor = .white
+        view.addSubview(imgViewFrame)
+        imgViewFrame.addSubview(photoImg)
+        view.addSubview(explanationLabel)
+        view.addSubview(resultView)
+        resultView.addSubview(predictionLabel)
+        view.addSubview(barChartView)
+        
+        imgViewFrame.snp.makeConstraints{
+            $0.width.height.equalTo(120)
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(30.0)
+            $0.leading.equalToSuperview().offset(20.0)
+        }
+        photoImg.snp.makeConstraints{
+            $0.width.height.equalTo(100)
+            $0.center.equalToSuperview()
+        }
+        explanationLabel.snp.makeConstraints{
+            $0.trailing.equalToSuperview().offset(10.0)
+            $0.centerY.equalTo(imgViewFrame.snp.centerY)
+            $0.leading.equalTo(imgViewFrame.snp.trailing).offset(20.0)
+        }
+        resultView.snp.makeConstraints{
+            $0.top.equalTo(imgViewFrame.snp.bottom).offset(30.0)
+            $0.centerX.equalToSuperview()
+            $0.width.height.equalTo(150)
+        }
+        predictionLabel.snp.makeConstraints{
+            $0.center.equalToSuperview()
+        }
+        barChartView.snp.makeConstraints{
+            $0.top.equalTo(resultView.snp.bottom).offset(30.0)
+            $0.centerX.equalToSuperview()
+            $0.width.height.equalTo(400)
+        }
+        
+    }
+    
+    // MARK: - objc Functions
+    
 }
